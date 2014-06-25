1d6852c674744b3c6ae31af1d353a237cf25236d
Callie Burke <calliejamesburke@gmail.com>
2014-06-24 19:42:49 -0700

    Moved around sitemap

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,5 +1,9 @@
 class UpdatesController < ApplicationController
   def index
+
+  	@countries = Country.where(:user_id => current_user)
+  	@myupdates = Update.get_feedzilla_country_ids_by_country("Benin")
+  	
   	@updates = Update.get_all_updates
 
   	@feedzillacountries = Update.get_feedzilla_countries
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+  	@updates = Update.get_all_updates
   end
 end
